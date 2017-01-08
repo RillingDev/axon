6f6aac30416b5fde3e1a4388e495809cb977d985
TheBinaryBee <coolicooli1@hotmail.de>
2017-01-08 12:28:03 +0100

    smaller fixes

--- dist/axon.es.js
+++ dist/axon.es.js
@@ -241,7 +241,7 @@ const renderModel = function (instance, node, propName) {
 const renderBind = function (instance, node, bindType, expression) {
     const propValue = evaluateExpression(instance, expression);
 
-    node.setAttribute(bindType,propValue);
+    node.setAttribute(bindType, propValue);
 
     return true;
 };
--- src/render/renderBind.js
+++ src/render/renderBind.js
@@ -5,7 +5,7 @@ import evaluateExpression from "../controller/evaluateExpression";
 const renderBind = function (instance, node, bindType, expression) {
     const propValue = evaluateExpression(instance, expression);
 
-    node.setAttribute(bindType,propValue);
+    node.setAttribute(bindType, propValue);
 
     return true;
 };
