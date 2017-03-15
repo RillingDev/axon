9ccd37d0bae7eb74a66a1804ec6b3350088cd291
FelixRilling <contact@f-rilling.com>
2017-03-15 11:36:20 +0100

    formatting changes

--- dist/axon.common.js
+++ dist/axon.common.js
@@ -130,14 +130,14 @@ const directiveIfRender = function (node, directive,instanceContent) {
  * @param {Boolean} immediate if the debounc should be ignored
  * @returns {Function} debounced function
  */
-const debounce = function(fn, wait, immediate) {
+const debounce = function (fn, wait, immediate) {
     let timeout;
 
-    return function() {
+    return function () {
         const context = this;
         const args = Array.from(arguments);
         const callNow = immediate && !timeout;
-        const later = function() {
+        const later = function () {
             timeout = null;
             if (!immediate) {
                 fn.apply(context, args);
