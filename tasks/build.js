25e42d8347ae42e815eb77b4e4b4192a7f437e7d
Felix Rilling <contact@f-rilling.com>
2017-06-15 18:17:45 +0200

    improved compression

--- tasks/build.js
+++ tasks/build.js
@@ -1,7 +1,7 @@
 "use strict";
 
-const fs = require("fs");
 const rollup = require("rollup");
+const replace = require("rollup-plugin-replace");
 const babel = require("babel-core");
 const uglify = require("uglify-es");
 const packageJson = require("../package.json");
@@ -30,10 +30,40 @@ rollup
             moduleName: packageJson.namespace.module,
             format: "iife"
         }).code).code;
-        const result_iife_min = uglify.minify(result_iife).code;
 
         saveOutput(`${DIR_DIST}/${packageJson.namespace.file}.esm.js`, result_es, "JS:ES");
         saveOutput(`${DIR_DIST}/${packageJson.namespace.file}.common.js`, result_cjs, "JS:CommonJS");
         saveOutput(`${DIR_DIST}/${packageJson.namespace.file}.js`, result_iife, "JS:IIFE");
+    });
+
+rollup
+    .rollup({
+        entry: `${DIR_SRC}/main.js`,
+        plugins: [replace({
+            "_content": "_a",
+            "_name": "_b",
+            "_opt": "_c",
+
+            "_element": "_d",
+            "_parent": "_e",
+            "_children": "_f",
+
+            "_val": "_g",
+            "_container": "_h",
+            "_key": "_i",
+            "_node": "_j",
+            "_args": "_k",
+        })]
+    })
+    .catch(err => {
+        console.log(err);
+    })
+    .then(bundle => {
+        const result_iife = babel.transform(bundle.generate({
+            moduleName: packageJson.namespace.module,
+            format: "iife"
+        }).code).code;
+        const result_iife_min = uglify.minify(result_iife).code;
+
         saveOutput(`${DIR_DIST}/${packageJson.namespace.file}.min.js`, result_iife_min, "JS:IIFE-min");
     });
